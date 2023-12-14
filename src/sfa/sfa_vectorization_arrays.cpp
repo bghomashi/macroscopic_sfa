75c7870f1fec3f9225b9d1001a4528ef3609c920
bghomashi <bghomashi@gmail.com>
2023-12-14 16:02:40 -0700

    sfa only option

--- src/sfa/sfa_vectorization_arrays.cpp
+++ src/sfa/sfa_vectorization_arrays.cpp
@@ -5,26 +5,28 @@
 
 namespace SFA {
     void SFA::SetupVectorization() {
+        int NTs = ts.size() * (ts.size() + 1) / 2 * sizeof(double);
+        LOG_INFO("allocating: " + std::to_string(13*NTs + (ts.size() * sizeof(cvec2))) + " bytes (" + std::to_string(double(13*NTs + (ts.size() * sizeof(cvec2)))/1024./1024./1024.) + " Gb" + ")");
 #if defined(PROFILING)
         Profile::Push("SFA::SetupVectorization");
 #endif
         dip = (cvec2*)fast::aligned_malloc(ts.size() * sizeof(cvec2), 32);
-        realx = (double*)fast::aligned_malloc(ts.size() * (ts.size() + 1) / 2 * sizeof(double), 32);
-        imagx = (double*)fast::aligned_malloc(ts.size() * (ts.size() + 1) / 2 * sizeof(double), 32);
-        realy = (double*)fast::aligned_malloc(ts.size() * (ts.size() + 1) / 2 * sizeof(double), 32);
-        imagy = (double*)fast::aligned_malloc(ts.size() * (ts.size() + 1) / 2 * sizeof(double), 32);
+        realx = (double*)fast::aligned_malloc(NTs, 32);
+        imagx = (double*)fast::aligned_malloc(NTs, 32);
+        realy = (double*)fast::aligned_malloc(NTs, 32);
+        imagy = (double*)fast::aligned_malloc(NTs, 32);
 
-        dtm_iti_real_x = (double*)fast::aligned_malloc(ts.size() * (ts.size() + 1) / 2 * sizeof(double), 32);
-        dtm_iti_imag_x = (double*)fast::aligned_malloc(ts.size() * (ts.size() + 1) / 2 * sizeof(double), 32);
-        dtm_iti_real_y = (double*)fast::aligned_malloc(ts.size() * (ts.size() + 1) / 2 * sizeof(double), 32);
-        dtm_iti_imag_y = (double*)fast::aligned_malloc(ts.size() * (ts.size() + 1) / 2 * sizeof(double), 32);
+        dtm_iti_real_x = (double*)fast::aligned_malloc(NTs, 32);
+        dtm_iti_imag_x = (double*)fast::aligned_malloc(NTs, 32);
+        dtm_iti_real_y = (double*)fast::aligned_malloc(NTs, 32);
+        dtm_iti_imag_y = (double*)fast::aligned_malloc(NTs, 32);
 
-        dtm_itr_real_x = (double*)fast::aligned_malloc(ts.size() * (ts.size() + 1) / 2 * sizeof(double), 32);
-        dtm_itr_imag_x = (double*)fast::aligned_malloc(ts.size() * (ts.size() + 1) / 2 * sizeof(double), 32);
-        dtm_itr_real_y = (double*)fast::aligned_malloc(ts.size() * (ts.size() + 1) / 2 * sizeof(double), 32);
-        dtm_itr_imag_y = (double*)fast::aligned_malloc(ts.size() * (ts.size() + 1) / 2 * sizeof(double), 32);
+        dtm_itr_real_x = (double*)fast::aligned_malloc(NTs, 32);
+        dtm_itr_imag_x = (double*)fast::aligned_malloc(NTs, 32);
+        dtm_itr_real_y = (double*)fast::aligned_malloc(NTs, 32);
+        dtm_itr_imag_y = (double*)fast::aligned_malloc(NTs, 32);
 
-        S0_saddle = (double*)fast::aligned_malloc(ts.size() * (ts.size() + 1) / 2 * sizeof(double), 32);
+        S0_saddle = (double*)fast::aligned_malloc(NTs, 32);
 
 #if defined(PROFILING)
         Profile::Pop("SFA::SetupVectorization");
