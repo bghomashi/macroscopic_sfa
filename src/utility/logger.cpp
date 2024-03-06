39ae3f0e27470e5bc98e8aeff19ce1a48c1ad7bc
bghomashi <bghomashi@gmail.com>
2024-03-05 23:52:35 -0700

    fixed log file

--- src/utility/logger.cpp
+++ src/utility/logger.cpp
@@ -6,25 +6,25 @@ static std::ofstream s_logger;
 
 namespace Log {
     void info(const std::string& text) {
-        if (s_logger.bad())
+        if (!s_logger.bad())
             s_logger << text << "\n";
         else
             std::cout << "INFO: " << text << "\n";
     }
     void warn(const std::string& text) {
-        if (s_logger.bad())
+        if (!s_logger.bad())
             s_logger << text << "\n";
         else
             std::cout << "WARN: " << text << "\n";
     }
     void critical(const std::string& text) {
-        if (s_logger.bad())
+        if (!s_logger.bad())
             s_logger << text << "\n";
         else
             std::cout << "CRITICAL: " << text << "\n";
     }
     void debug(const std::string& text) {
-        if (s_logger.bad())
+        if (!s_logger.bad())
             s_logger << text << "\n";
         else
             std::cout << "DEBUG: " << text << "\n";
